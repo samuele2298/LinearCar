4eddb50422feca8c24de4c96274ddf55a51da0f6
samuele2298 <40797549+samuele2298@users.noreply.github.com>
2024-07-16 19:26:05 +0200

    s

--- src/components/OfferSlider.tsx
+++ src/components/OfferSlider.tsx
@@ -8,7 +8,7 @@ import './Slider.css';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
-
+/* 
 <Carousel
     showArrows={true}
     selectedItem={currentSlide}
@@ -24,45 +24,45 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
             />
         </div>
     ))}</Carousel>
-
+ */
 
 const Slider = () => {
 
-
-    useEffect(() => {
-        const trandingSlider = new Swiper('.tranding-slider', {
-            effect: 'coverflow',
-            grabCursor: true,
-            centeredSlides: true,
-            loop: true,
-            slidesPerView: 'auto',
-            coverflowEffect: {
-                rotate: 0,
-                stretch: 0,
-                depth: 100,
-                modifier: 2.5,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        });
-    }, []);
-
+    /* 
+        useEffect(() => {
+            const trandingSlider = new Swiper('.tranding-slider', {
+                effect: 'coverflow',
+                grabCursor: true,
+                centeredSlides: true,
+                loop: true,
+                slidesPerView: 'auto',
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 2.5,
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+            });
+        }, []);
+     */
     return (
 
 
         <Carousel
             showArrows={true}
-            selectedItem={currentSlide}
-            onChange={handleSlideChange}
+            /*    selectedItem={currentSlide}
+               onChange={handleSlideChange} */
             showThumbs={false}
         >
-            {car.images.map((image, index) => (
+            {/*   {car.images.map((image, index) => (
                 <div key={`${car.id}-${index}`}>
                     <img
                         src={`cars/${car.id}/${image}`}
@@ -70,7 +70,7 @@ const Slider = () => {
                         className="w-full rounded-lg shadow-lg"
                     />
                 </div>
-            ))}</Carousel>
+            ))} */}</Carousel>
     );
 };
 
